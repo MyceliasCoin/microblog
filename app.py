87a12622695bc63624655841f08cc0394a0029f9
MyceliasCoin <shroomcoiner@gmail.com>
2019-02-24 21:49:45 -0500

    Initial commit

---
+++ app.py
@@ -0,0 +1,47 @@
+from flask import Flask
+from flask import render_template, flash, redirect, url_for
+from config import Config
+from static.forms import LoginForm
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+from static import models
+
+# print(app.config['SECRET_KEY'])
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'username': 'Giorno'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
+
+
+if __name__ == '__main__':
+    app.run()
+
